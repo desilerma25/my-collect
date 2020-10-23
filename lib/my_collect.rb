e0bd8ee1386c5826c4e2d4c93b775e628c3cf219
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-23 18:54:56 -0500

    commit tree 42df0244aa290f0207b928321f23b47b2c41856a

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array) # method takes in arg.
+    i = 0 # set counter to 0
+    collection = [] # create an empty array for new values
+    while i < array.length # while loop to run through each element in array
+        collection << yield(array[i]) # shovel each new array element into the new collection array
+        i += 1 # increment each index by 1 to get through all
+    end # close out the while loop
+    collection # return the collection array. now contains new values
+end # close entire method
